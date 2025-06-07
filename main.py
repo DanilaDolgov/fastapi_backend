51ac2e461d6fb9fdfee1762b14541b9277705b16
DANILEVICH <danila@MacBook-Pro-Danila.local>
2025-06-07 17:13:19 +0300

    added putch and put method for hotels

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Body
 from fastapi.openapi.docs import get_swagger_ui_html
+from typing import Optional
 import uvicorn
 
 app = FastAPI(docs_url=None)
 
 hotels = [
-    {"id": 1, "title": "Sochi"},
-    {"id": 2, "title": "Дубай"},
+    {"id": 1, "title": "Sochi", "name": "Sochi", "address": "RUSSIA, KRASNODARSKII KRAI, SOCHI", "phone": 7999999999},
+    {"id": 2, "title": "Дубай", "name": "Дубай", "address": "UAE, DUBAI", "phone": 97100000000},
 ]
+
 
 @app.get("/")
 def func():
     return "Hello World!!!!!!!!!!"
+
 
 @app.get("/hotels")
 def get_hotels(
@@ -27,6 +30,64 @@ def get_hotels(
             continue
         hotels_.append(hotel)
     return hotels_
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotels(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {'Status': 'Ok'}
+
+
+@app.post("/hotels")
+def create_hotels(
+        title: str = Body(embed=True)
+):
+    global hotels
+    hotels.append(
+        {
+            "id": hotels[-1]["id"] + 1,
+            "title": title
+        }
+    )
+    return {'Status': 'Ok'}
+
+
+@app.put("/hotels/{hotel_id}")
+def update_hotel(
+        hotel_id: int,
+        title: str = Body(embed=True),
+        name: str = Body(embed=True),
+        address: str = Body(embed=True),
+        phone: int = Body(embed=True)
+):
+    global hotels
+    hotels = [{**hotel, "title": title, "name": name, "address": address, "phone": phone} if
+              hotel["id"] == hotel_id else hotel for hotel in hotels]
+    return {"Status": "Ok"}
+
+
+@app.patch("/hotels/{hotel_id}")
+def update_patch_hotel(
+        hotel_id: int,
+        title: Optional[str] = Body(default=None),
+        name: Optional[str] = Body(default=None),
+        address: Optional[str] = Body(default=None),
+        phone: Optional[int] = Body(default=None)
+
+):
+    global hotels
+    hotels = [
+        {
+            **hotel,
+            "title": title if title is not None else hotel["title"],
+            "name": name if name is not None else hotel["name"],
+            "address": address if address is not None else hotel["address"],
+            "phone": phone if phone is not None else hotel["phone"]
+        } if hotel["id"] == hotel_id else hotel
+        for hotel in hotels
+    ]
+    return {"Status": "Ok"}
 
 
 @app.get("/docs", include_in_schema=False)
