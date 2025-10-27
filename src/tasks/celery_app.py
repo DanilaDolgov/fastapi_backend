7973a281e6845f9c2e42f205af62d1c293ed2267
DANILEVICH <danila@MacBook-Pro-Danila.local>
2025-10-27 13:18:42 +0300

    added test

--- src/tasks/celery_app.py
+++ src/tasks/celery_app.py
@@ -9,6 +9,6 @@ celery_instance = Celery("tasks", broker=settings.DB_REDIS, include=["src.tasks.
 celery_instance.conf.beat_schedule = {
     "send_mail_checkin": {
         "task": "booking_today_checkin",  # имя задачи
-        "schedule": crontab(hour=0, minute=0),  # каждый день в полночь
+        "schedule": crontab(minute="*/5"),  # каждый день в полночь
     },
 }
